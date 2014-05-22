e312998e0f27ff95a4dde35ec9262023368f8267
godai_0519 <godai0519@gmail.com>
2014-05-22 20:21:32 +0900

    question_data/question_raw_dataはmove可能，copy不可能クラスへ．
    Signed-off-by: godai_0519 <godai0519@gmail.com>

--- resolver/src/data_type.hpp
+++ resolver/src/data_type.hpp
@@ -24,25 +24,47 @@ typedef std::vector<std::vector<pixel_type>> image_type;
 // 気持ち悪いが，[i][j]の位置に分割された画像が入っている．更に[j][k]へのアクセスによって画素にアクセス
 typedef std::vector<std::vector<image_type>> split_image_type;
 
-struct question_data
+struct question_data : private boost::noncopyable
 {
     std::pair<int,int> size;
     int selecrtable;
     int cost_select;
     int cost_change;
-
     std::vector<std::vector<point_type>> block;
+
+    question_data(
+        std::pair<int,int> const& size,
+        int const selecrtable,
+        int const cost_select,
+        int const cost_change,
+        std::vector<std::vector<point_type>> const& block
+        )
+        : size(size), selecrtable(selecrtable), cost_select(cost_select), cost_change(cost_change), block(block)
+    {
+    }
+
+    question_data(question_data&& other)
+    {
+        *this = std::move(other);
+    }
+    question_data& operator=(question_data&& other)
+    {
+        this->size        = std::move(other.size);
+        this->selecrtable = other.selecrtable;
+        this->cost_select = other.cost_select;
+        this->cost_change = other.cost_change;
+        this->block       = std::move(other.block);
+        return *this;
+    }
 };
 
-struct question_raw_data : boost::noncopyable
+struct question_raw_data : private boost::noncopyable
 {
-public:
     std::pair<int,int> split_num; // x * y
     int selectable_num;
     std::pair<int,int> cost; // 選択コスト / 交換コスト
     std::pair<int,int> size; // x * y
     int max_brightness; // 最大輝度
-
     image_type pixels;
     
     question_raw_data()
