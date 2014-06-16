04a5628e4c6cd23b6fb4bb621386258db9a32996
godai_0519 <godai0519@gmail.com>
2014-06-16 23:30:47 +0900

    FLTKの表示部分だけできた．リンクが失敗する場合がある現象が不明．
    Signed-off-by: godai_0519 <godai0519@gmail.com>

---
+++ clammbon/include/gui.hpp
@@ -0,0 +1,13 @@
+﻿#ifndef CLAMMBON_GUI_HPP
+#define CLAMMBON_GUI_HPP
+
+#include <future>
+#include "data_type.hpp"
+
+namespace gui
+{
+    std::future<int> make_window(image_type const& image);
+} // namespace gui
+
+#endif
+
