05a324cb0d460734276fd5e5861fe30fd3f9cc01
godai_0519 <godai0519@gmail.com>
2014-06-20 23:00:50 +0900

    パズドラ的な動きをするGUIを提供するクラス． ただ，まだ情報収集をするようになっていないので，何も返せない．
    Signed-off-by: godai_0519 <godai0519@gmail.com>

--- clammbon/include/gui.hpp
+++ clammbon/include/gui.hpp
@@ -3,10 +3,37 @@
 
 #include <future>
 #include "data_type.hpp"
+#include "splitter.hpp"
 
 namespace gui
 {
     std::future<int> make_window(image_type const& image, std::string const& window_name = "Untitled");
+
+    class MoveBox : public Fl_Box
+    {
+    public:
+        MoveBox(int const x, int const y, int const w, int const h);
+        virtual ~MoveBox() = default;
+
+    private:
+        void position_swap(MoveBox* lhs, MoveBox* rhs);
+        int handle(int const event) override;
+    };
+    
+    class MoveWindow : public Fl_Window
+    {
+    public:
+        MoveWindow(split_image_type const& images, std::string const& window_name);
+        virtual ~MoveWindow() = default;
+    
+    private:
+        splitter sp_;
+        std::string const window_name_;
+        std::vector<std::vector<unfold_image_type>> image_;
+        std::vector<std::vector<Fl_RGB_Image*>> rgbs_;
+        std::vector<std::vector<std::unique_ptr<MoveBox>>> boxes_;
+    };
+
 } // namespace gui
 
 #endif
--- clammbon/src/gui.cpp
+++ clammbon/src/gui.cpp
@@ -1,4 +1,5 @@
 ﻿#include <memory>
+#include <boost/format.hpp>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
@@ -31,5 +32,117 @@ namespace gui
             }
         );
     }
+
+    MoveBox::MoveBox(int const x, int const y, int const w, int const h)
+        : Fl_Box(x, y, w, h)
+    {
+    }
+
+    void MoveBox::position_swap(MoveBox* lhs, MoveBox* rhs)
+    {
+        int const x = lhs->x();
+        int const y = lhs->y();
+        lhs->position(rhs->x(), rhs->y());
+        rhs->position(x, y);
+        lhs->redraw();
+        rhs->redraw();
+    }
+
+    int MoveBox::handle(int const event)
+    {
+        int ret = Fl_Box::handle(event);
+        switch(event)
+        {
+            case FL_DND_ENTER:
+            {
+                std::string text = Fl::event_text();
+                if(text.size() == 0) break;
+
+                MoveBox* before = reinterpret_cast<MoveBox*>(std::stoull(text, nullptr, 16));
+                position_swap(before, this);
+                ret =1;
+                break;
+            }
+
+            case FL_DND_DRAG:
+            case FL_DND_RELEASE:
+                ret = 1;
+                break;
+
+            case FL_PUSH:
+            {
+                std::string const msg = (boost::format("0x%016x") % reinterpret_cast<uint64_t>(this)).str();
+                Fl::copy(msg.c_str(), msg.size(), 0);
+                Fl::dnd();
+                ret = 1;
+                break;
+            }
+
+            case FL_PASTE:
+                ret = 1;
+                break;
+        }
+
+        return ret;
+    }
+
+    MoveWindow::MoveWindow(split_image_type const& images, std::string const& window_name)
+        : Fl_Window(/*temporary size*/10, 10), window_name_(window_name)
+    {
+        int const image_width   = images[0][0][0].size();
+        int const image_height  = images[0][0]   .size();
+        int const split_x       = images[0]      .size();
+        int const split_y       = images         .size();
+        int const window_width  = split_x * image_width;
+        int const window_height = split_y * image_height;
+
+        this->size(window_width, window_height);
+        this->label(window_name_.c_str());
+
+        // 引数の「2次元配置された2次元イメージ」を「2次元配置された1次元イメージ」に変える
+        image_.clear();
+        image_.reserve(split_y);
+        for(int i=0; i<split_y; ++i)
+        {
+            std::vector<unfold_image_type> line;
+            line.reserve(split_x);
+            for(int j=0; j<split_x; ++j)
+            {
+                line.push_back(sp_.unfold_image(images[i][j]));
+            }
+            image_.push_back(std::move(line));
+        }
+
+        // 「1次元配置された1次元イメージ」をFLTK用のイメージにリンクさせる．(どちらも生きている必要がある)
+        rgbs_.clear();
+        rgbs_.reserve(split_y);
+        for(int i=0; i<split_y; ++i)
+        {
+            std::vector<Fl_RGB_Image*> line;
+            line.reserve(split_x);
+            for(int j=0; j<split_x; ++j)
+            {
+                line.push_back(new Fl_RGB_Image(image_[i][j].data(), image_width, image_height));
+            }
+            rgbs_.push_back(std::move(line));
+        }
+
+        // FLTKイメージをMoveBoxに関連付ける
+        boxes_.clear();
+        boxes_.reserve(split_y);
+        for(int i=0; i<split_y; ++i)
+        {
+            std::vector<std::unique_ptr<MoveBox>> line;
+            line.reserve(split_x);
+            for(int j=0; j<split_x; ++j)
+            {
+                auto box = std::make_unique<MoveBox>(image_width*j, image_height*i, image_width, image_height);
+                box->image(rgbs_[i][j]);
+                line.push_back(std::move(box));
+            }
+            boxes_.push_back(std::move(line));
+        }
+    }
+
 } // namespace gui
 
