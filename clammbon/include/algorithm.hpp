29c8c3f1da0b5aaad685ccc114be78c5b7358685
Mizki SUZUMORI <mizki9577@gmail.com>
2014-06-24 01:09:35 +0900

    コミットログ汚して申し訳ないがやり直し

--- clammbon/include/algorithm.hpp
+++ clammbon/include/algorithm.hpp
@@ -11,13 +11,8 @@ public:
 
     void operator() (question_data const& data);
 
-    int manhattan_distance(point_type& p0, point_type& p1);
-    int distance_to_origial(point_type& point);
-    bool check_finish();
-
 private:
     std::vector<std::vector<point_type>> matrix;
-    direction_type<int>* directions;
 };
 
 #endif
--- clammbon/src/algorithm.cpp
+++ clammbon/src/algorithm.cpp
@@ -7,39 +7,8 @@ void algorithm::operator() (question_data const& data)
     // Main Algorithm
     // 正しい順序に並べてみたり，データ送ったり．
     //
+    // Ian Parberry "A Real-Time Algorithm for the (n^2-1)-Puzzle"
+    //
 
     matrix = data.block;
-    direction_type<int>* directions = new direction_type<int>;
-    directions->up = 0, directions->right = 1, directions->down = 2, directions->left = 3;
-}
-
-int algorithm::manhattan_distance(point_type& p0, point_type& p1)
-{
-    // 2つの座標を受け取ってマンハッタン距離を返す
-    return abs(p0.x - p1.x) + abs(p0.y - p1.y);
-}
-
-int algorithm::distance_to_origial(point_type& point)
-{
-    // 1つの座標を受け取って, その座標に存在する断片画像の, 原座標までのマンハッタン距離を返す
-    return manhattan_distance(point, matrix[point.y][point.x]);
-}
-
-bool algorithm::check_finish()
-{
-    // 終了判定
-    bool finished = true;
-    for (std::vector<point_type> row : matrix) {
-        for (point_type point : row) {
-            if (distance_to_origial(point) != 0) {
-                finished = false;
-            }
-            break;
-        }
-        if (!finished) {
-            break;
-        }
-    }
-
-    return finished;
 }
