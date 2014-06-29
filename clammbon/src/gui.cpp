571e939258246ae97f4f725540aacbf4e727df6e
godai_0519 <godai0519@gmail.com>
2014-06-29 14:06:56 +0900

    gui::MoveWindowをラップした関数を作成．従って，gui::MoveWindowを隠蔽．
    Signed-off-by: godai_0519 <godai0519@gmail.com>

--- clammbon/src/gui.cpp
+++ clammbon/src/gui.cpp
@@ -34,6 +34,37 @@ namespace gui
         );
     }
 
+    class MoveBox : public Fl_Box
+    {
+    public:
+        MoveBox(point_type const& point, int const x, int const y, int const w, int const h);
+        virtual ~MoveBox() = default;
+
+        point_type const& point() const;
+
+    private:
+        void position_swap(MoveBox* lhs, MoveBox* rhs);
+        int handle(int const event) override;
+
+        point_type point_;
+    };
+
+    class MoveWindow : public Fl_Window
+    {
+    public:
+        MoveWindow(split_image_type const& images, std::string const& window_name);
+        virtual ~MoveWindow() = default;
+
+        std::vector<std::vector<point_type>> result() const;
+
+    private:
+        splitter sp_;
+        std::string const window_name_;
+        std::vector<std::vector<unfold_image_type>> image_;
+        std::vector<std::vector<Fl_RGB_Image*>> rgbs_;
+        std::vector<std::vector<std::unique_ptr<MoveBox>>> boxes_;
+    };
+
     MoveBox::MoveBox(point_type const& point, int const x, int const y, int const w, int const h)
         : Fl_Box(x, y, w, h), point_(point)
     {
@@ -174,6 +205,27 @@ namespace gui
         }
 
         return result;
+    }
+    
+    std::future<std::vector<std::vector<point_type>>> make_mansort_window(
+        question_raw_data const& data,
+        std::string const& window_name
+        )
+    {
+        splitter sp;
+        auto splitted = sp.split_image(data);
+
+        return std::async(
+            std::launch::async,
+            [=, splitted=std::move(splitted)]() -> std::vector<std::vector<point_type>>
+            {
+                MoveWindow window(splitted, window_name);
+                window.show();
+
+                Fl::run();
+                return window.result();
+            }
+        );
     }
 } // namespace gui
 
