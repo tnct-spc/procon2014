dcdf47af3b14964b345bdc32b056f4d8e3962c7a
Godai Azuma <godai0519@gmail.com>
2014-08-18 21:36:38 +0900

    #2 Boost.Contextではないが，Boost.Coroutineを利用して逐次処理をalgorithmクラスに実装してみた．
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

--- clammbon/include/algorithm.hpp
+++ clammbon/include/algorithm.hpp
@@ -1,15 +1,27 @@
 ﻿#ifndef RESOLVER_ALGORITHM_HPP
 #define RESOLVER_ALGORITHM_HPP
 
+#include <boost/optional.hpp>
+#include <boost/coroutine/coroutine.hpp>
 #include "data_type.hpp"
 
 class algorithm
 {
 public:
+    typedef answer_list return_type; // TODO: あとで相談とか
+
     algorithm() = default;
     virtual ~algorithm() = default;
 
-    void operator() (question_data const& data);
+    auto get() -> boost::optional<return_type>;
+    void reset(question_data const& data);
+
+private:
+    void process(boost::coroutines::coroutine<return_type>::push_type& yield);
+    void operator() (boost::coroutines::coroutine<return_type>::push_type& yield);
+
+    boost::optional<question_data> data_;
+    boost::coroutines::coroutine<return_type>::pull_type co_;
 };
 
 #endif
