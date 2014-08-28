e6459ecd052574d2352c847779becafd04d7a587
吉川 千里 <tisato8899@mail.com>
2014-08-29 00:41:45 +0900

    moronimatigaeteta

--- clammbon/src/sort_algorithm/compare.cpp
+++ clammbon/src/sort_algorithm/compare.cpp
@@ -80,18 +80,18 @@ point_type dl_choose(compared_type const& comp, point_type const ul, point_type 
 uint64_t form_evaluate(compared_type const& comp_, std::vector<std::vector<point_type> > matrix)
 {
 	uint64_t s = 0;
-	const int sepx = matrix[0].size();
-	const int sepy = matrix.size();
+	const int width = matrix[0].size();
+	const int height = matrix.size();
 
-	for (int i = 0; i < sepy; ++i)for (int j = 0; j < sepx; ++j){
-		if (i != sepy - 1) s += comp_[matrix[i][j].y][matrix[i][j].x][matrix[i + 1][j].y][matrix[i + 1][j].x].down;
-		if (j != sepx - 1) s += comp_[matrix[i][j].y][matrix[i][j].x][matrix[i][j + 1].y][matrix[i][j + 1].x].right;
+	for (int i = 0; i < height; ++i)for (int j = 0; j < width; ++j){
+		if (i != height - 1) s += comp_[matrix[i][j].y][matrix[i][j].x][matrix[i + 1][j].y][matrix[i + 1][j].x].down;
+		if (j != width - 1) s += comp_[matrix[i][j].y][matrix[i][j].x][matrix[i][j + 1].y][matrix[i][j + 1].x].right;
 	}
 
 	return s;
 }
 
-//指定された範囲内の問題画像の種類を返す関数
+/*指定された範囲内の問題画像の種類を返す関数*/
 int get_kind_num(question_raw_data const& data_,std::vector<std::vector<point_type>> matrix, int x, int y){
 	int const sepx = data_.split_num.first;
 	int const sepy = data_.split_num.second;
@@ -99,7 +99,7 @@ int get_kind_num(question_raw_data const& data_,std::vector<std::vector<point_ty
 	std::vector<point_type> temp;
 	for (int i = 0; i < sepy; i++){
 		for (int j = 0; j < sepx; j++){
-			temp.push_back(matrix[j][i]);
+			temp.push_back(matrix[x + j][y + i]);
 		}
 	}
 	std::sort(temp.begin(), temp.end());
