66ebf325619dfed544fbfca6de345b10655b3d37
imoneg <kazu.takishima@gmail.com>
2014-08-30 10:47:26 +0900

    村上剛基のアルゴリズムを実装中

---
+++ clammbon/include/sort_algorithm/Murakami.hpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <data_type.hpp>
+//人名(村上剛基)なのでキャメルケースです
+class Murakami
+{
+private:
+	typedef std::vector<std::vector<point_type>> return_type;
+	question_raw_data const& data_;
+	compared_type const& comp_;
+
+	//ブロック(分割画像のかたまり),サイズは分割数(m,n)で固定,空白(分割画像の入らない部分)にはpoint_type(x = -1,y = -1)が入る
+	typedef std::vector<std::vector<point_type>> block_type;//サイズを固定し,point_typeを初期化する
+	//作ったブロックをしまっておくlist,この要素が1になったら復元画像完成(ただしはみ出ないものとする)
+	//std::vector<block_type> block_list; //std::mapで一緒に評価値を持たせてもいいかも////cppに移動
+	//一枚目      二枚目  一枚目から見た向き 順位(10位までの11要素,0は自分)
+	//[point_type][UDRL][Rank]の順でアクセスする,中身は分割画像
+	std::vector < std::vector < std::vector<point_type>>> sorted_comparation;//これのラッパー関数があったほうがいい?
+	//eval_blockの返り値のための構造体
+	struct block_combination{
+		//ここでブロックへの参照を保持しないと結合後のブロックを消せない
+		block_type block1;
+		block_type block2;
+		//あるブロックとの結合するとき(結合したあと)の評価値
+		std::int_fast64_t score; //評価値は結構大きいので32bitだとオーバーフローするかも?
+	};
+	//ブロック対ブロックの評価,ブロックに持たせるべき?
+	block_combination eval_block(block_type, block_type);
+	//ピース対ピースの評価,仕様をよく理解しないと書くのが辛い
+	std::int_fast64_t eval_piece(point_type, point_type);
+	block_type combine_block(block_type, block_type);
+public:
+	Murakami(question_raw_data const& data, compared_type const& comp);
+	virtual ~Murakami() = default;
+	std::vector<std::vector<std::vector<point_type>>> operator() ();
+};
---
+++ clammbon/src/sort_algorithm/Murakami.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <boost/format.hpp>
+#include <boost/range/algorithm_ext/erase.hpp>
+#include <vector>
+#include <algorithm>
+#include <data_type.hpp>
+#include <sort_algorithm/compare.hpp>
+#include <sort_algorithm/adjacent.hpp>
+#include <sort_algorithm/Murakami.hpp>
+
+Murakami::Murakami(question_raw_data const& data, compared_type const& comp)
+	: data_(data), comp_(comp)
+{
+}
+
+std::vector<std::vector<std::vector<point_type>>> Murakami::operator() (){
+	auto const width = data_.split_num.first;
+	auto const height = data_.split_num.second;
+	//block_type block(height, std::vector<point_type>(width, { -1, -1 }));
+	//大きさが[height * width][height][width]で中身がx,y = -1で初期化
+	std::vector<std::vector<std::vector<point_type>>> block_list(
+		height * width,
+		std::vector<std::vector<point_type>>(
+			height,
+			std::vector<point_type>(
+				width,
+				{
+					-1, -1
+				}
+				)
+
+				)
+				);
+
+	//ブロックの左上に分割画像を配置するループ
+	for (int i = 0; i < height; i++){
+		for (int j = 0; j < width; j ++){
+			block_list[i * width + j][0][0].y = i;
+			block_list[i * width + j][0][0].x = j;
+		}
+	}
+
+	while (block_list.size() == 1){//メインループ
+		block_combination best_block_combination;//一番いいblockの組み合わせいれるところ
+		for (auto i : block_list){//ブロック同士を比較するループ
+			for (auto j : block_list){
+				if (i == j) continue;
+				block_combination b = eval_block(i, j);
+				if (best_block_combination.score < b.score){
+					best_block_combination.score = b.score; //TODO 同じ値があったらどうしよ
+				}
+			}
+		}
+		block_type combined_block = combine_block(best_block_combination.block1, best_block_combination.block2);//ブロックを結合する
+		boost::remove_erase_if(block_list, [best_block_combination](block_type it){//block_listから結合する前のブロックを消す
+			return (it == best_block_combination.block1 || it == best_block_combination.block2);
+		});
+		block_list.push_back(combined_block);//結合したのを入れる
+	}
+	return block_list;
+}
+Murakami::block_combination Murakami::eval_block(block_type block1, block_type block2){
+	auto const width = data_.split_num.first;
+	auto const height = data_.split_num.second;
+	block_combination comb;
+	comb.block1 = block1;
+	comb.block1 = block2;
+	auto const in_range = [height, width](int y, int x){
+		if (x >= 0 && x < width && y >= 0 && y < height){
+			return true;
+		}
+		else{
+			return false;
+		}
+	};
+	auto const exists = [height,width](int y, int x,block_type b){
+		if (x >= 0 && x < width && y >= 0 && y < height){
+			if (b[y][x].x != -1 || b[y][x].y != -1){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	};
+	for (int i = 0; i < height * 2 - 1; i++){
+		for (int j = 0; j < width * 2 - 1; j++){
+			for (int k = 0; k < height; k++){
+				for (int l = 0; l < width; l++){
+					if (exists(k,l,block1) && !exists(k + i, l + j,block2)){
+						int_fast64_t c = 0;
+						int_fast64_t rank1_num = 0;//キャストが面倒くさいからint_fast64_tで
+						if (exists(k + i - 1,l + j,block2)){//上
+							c += eval_piece(block1[k][l], block2[k + i - 1][l + j]);
+						}
+						if (exists(k + i, l + j - 1,block2)){//左
+
+						}
+						if (exists(k + i + 1, l + j,block2)){//下
+
+						}
+						if (exists(k + i, l + j + 1,block2)){//右
+
+						}
+					}
+				}
+			}
+		}
+	}
+}
