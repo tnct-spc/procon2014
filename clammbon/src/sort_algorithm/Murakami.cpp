fa2bca0fcf3614f0fe593519bf8e2488d4c5c217
imoneg <kazu.takishima@gmail.com>
2014-08-30 17:41:52 +0900

    コピペした

--- clammbon/src/sort_algorithm/Murakami.cpp
+++ clammbon/src/sort_algorithm/Murakami.cpp
@@ -3,7 +3,6 @@
 #include <boost/range/algorithm_ext/erase.hpp>
 #include <vector>
 #include <algorithm>
-#include <fstream>
 #include <data_type.hpp>
 #include <sort_algorithm/compare.hpp>
 #include <sort_algorithm/adjacent.hpp>
@@ -22,43 +21,43 @@ std::vector<std::vector<std::vector<point_type>>> Murakami::operator() (){
 	std::vector<std::vector<std::vector<point_type>>> block_list(
 		height * width,
 		std::vector<std::vector<point_type>>(
-			height,
-			std::vector<point_type>(
-				width,
-				{
-					-1, -1
-				}
-				)
-
-				)
-				);
-
-	//ブロックの左上に分割画像を配置するループ
-	for (int i = 0; i < height; i++){
-		for (int j = 0; j < width; j ++){
-			block_list[i * width + j][0][0].y = i;
-			block_list[i * width + j][0][0].x = j;
+		height,
+		std::vector<point_type>(
+		width,
+		{
+		-1, -1
 		}
-	}
+		)
 
-	while (block_list.size() == 1){//メインループ
-		block_combination best_block_combination;//一番いいblockの組み合わせいれるところ
-		for (auto i : block_list){//ブロック同士を比較するループ
-			for (auto j : block_list){
-				if (i == j) continue;
-				block_combination b = eval_block(i, j);
-				if (best_block_combination.score < b.score){
-					best_block_combination.score = b.score; //TODO 同じ値があったらどうしよ
-				}
+		)
+		);
+
+		//ブロックの左上に分割画像を配置するループ
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
+				block_list[i * width + j][0][0].y = i;
+				block_list[i * width + j][0][0].x = j;
 			}
 		}
-		block_type combined_block = combine_block(best_block_combination);//ブロックを結合する
-		boost::remove_erase_if(block_list, [best_block_combination](block_type it){//block_listから結合する前のブロックを消す
-			return (it == best_block_combination.block1 || it == best_block_combination.block2);
-		});
-		block_list.push_back(combined_block);//結合したのを入れる
-	}
-	return block_list;
+
+		while (block_list.size() == 1){//メインループ
+			block_combination best_block_combination;//一番いいblockの組み合わせいれるところ
+			for (auto i : block_list){//ブロック同士を比較するループ
+				for (auto j : block_list){
+					if (i == j) continue;
+					block_combination b = eval_block(i, j);
+					if (best_block_combination.score < b.score){
+						best_block_combination.score = b.score; //TODO 同じ値があったらどうしよ
+					}
+				}
+			}
+			block_type combined_block = combine_block(best_block_combination);//ブロックを結合する
+			boost::remove_erase_if(block_list, [best_block_combination](block_type it){//block_listから結合する前のブロックを消す
+				return (it == best_block_combination.block1 || it == best_block_combination.block2);
+			});
+			block_list.push_back(combined_block);//結合したのを入れる
+		}
+		return block_list;
 }
 Murakami::block_combination Murakami::eval_block(block_type block1, block_type block2){
 	auto const width = data_.split_num.first;
@@ -71,7 +70,7 @@ Murakami::block_combination Murakami::eval_block(block_type block1, block_type b
 			return false;
 		}
 	};
-	auto const exists = [height,width](int y, int x,block_type b){
+	auto const exists = [height, width](int y, int x, block_type b){
 		if (x >= 0 && x < width && y >= 0 && y < height){
 			if (b[y][x].x != -1 || b[y][x].y != -1){
 				return true;
@@ -98,24 +97,24 @@ Murakami::block_combination Murakami::eval_block(block_type block1, block_type b
 						if (block1[k][l].x != -1 && block1[k][l].y != -1 && block2[k - i][l - j].x != -1 && block2[k - i][l - j].y != -1){
 							confliction = true;
 							break;
-						} 
+						}
 					}
-					if (exists(k,l,block1) && !exists(k + i, l + j,block2)){
+					if (exists(k, l, block1) && !exists(k + i, l + j, block2)){
 						int_fast64_t piece_c = 0;
-						if (exists(k + i - 1,l + j,block2)){//上
-							piece_c += eval_piece(block1[k][l], block2[k + i - 1][l + j],up);
-							if (sorted_comparation[block1[k][l]][up][1] == block2[k + i - 1][l + j]) rank1_num ++;
+						if (exists(k + i - 1, l + j, block2)){//上
+							piece_c += eval_piece(block1[k][l], block2[k + i - 1][l + j], up);
+							if (sorted_comparation[block1[k][l]][up][1] == block2[k + i - 1][l + j]) rank1_num++;
 						}
-						if (exists(k + i, l + j - 1,block2)){//左
-							piece_c += eval_piece(block1[k][l], block2[k + i][l + j - 1],left);
-							if (sorted_comparation[block1[k][l]][left][1] == block2[k + i][l + j -1]) rank1_num++;
+						if (exists(k + i, l + j - 1, block2)){//左
+							piece_c += eval_piece(block1[k][l], block2[k + i][l + j - 1], left);
+							if (sorted_comparation[block1[k][l]][left][1] == block2[k + i][l + j - 1]) rank1_num++;
 						}
-						if (exists(k + i + 1, l + j,block2)){//下
-							piece_c += eval_piece(block1[k][l], block2[k + i + 1][l + j],down);
+						if (exists(k + i + 1, l + j, block2)){//下
+							piece_c += eval_piece(block1[k][l], block2[k + i + 1][l + j], down);
 							if (sorted_comparation[block1[k][l]][down][1] == block2[k + i + 1][l + j]) rank1_num++;
 						}
-						if (exists(k + i, l + j + 1,block2)){//右
-							piece_c += eval_piece(block1[k][l], block2[k + i][l + j + 1],right);
+						if (exists(k + i, l + j + 1, block2)){//右
+							piece_c += eval_piece(block1[k][l], block2[k + i][l + j + 1], right);
 							if (sorted_comparation[block1[k][l]][right][1] == block2[k + i][l + j + 1]) rank1_num++;
 						}
 						//pow(piece_c, rank1_num);
@@ -143,7 +142,63 @@ Murakami::block_combination Murakami::eval_block(block_type block1, block_type b
 	};
 	return return_struct;
 }
+std::int_fast64_t Murakami::eval_piece(point_type p1, point_type p2, direction dir){
+	int_fast64_t score = 0;
+	score += eval_comp_(p1, p2, dir);
+	score += eval_comp_(p2, p1, dir);
+	return score;
+}
+std::int_fast64_t Murakami::eval_comp_(point_type p1, point_type p2, direction dir){
+	int pos = 0;
+	std::int_fast64_t score = 0;
+	for (auto it : sorted_comparation[p1][dir]){
+		if (it == p2)break;
+		pos++;
+	}
+	if (pos == 0)throw std::runtime_error("断片画像が重複しています");
+	if (pos == 1){
+		switch (dir)
+		{
+		case Murakami::up:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][up][2].y][sorted_comparation[p1][up][2].x].up - comp_[p1.y][p1.x][p2.y][p2.x].up;
+			break;
+		case Murakami::right:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][right][2].y][sorted_comparation[p1][right][2].x].right - comp_[p1.y][p1.x][p2.y][p2.x].right;
+			break;
+		case Murakami::down:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][down][2].y][sorted_comparation[p1][down][2].x].down - comp_[p1.y][p1.x][p2.y][p2.x].down;
+			break;
+		case Murakami::left:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][left][2].y][sorted_comparation[p1][left][2].x].left - comp_[p1.y][p1.x][p2.y][p2.x].left;
+			break;
+		default:
+			break;
+		}
+	}
+	else if (pos > 1){
+		switch (dir)
+		{
+		case Murakami::up:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][up][1].y][sorted_comparation[p1][up][1].x].up - comp_[p1.y][p1.x][p2.y][p2.x].up;
+			break;
+		case Murakami::right:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][right][1].y][sorted_comparation[p1][right][1].x].right - comp_[p1.y][p1.x][p2.y][p2.x].right;
+			break;
+		case Murakami::down:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][down][1].y][sorted_comparation[p1][down][1].x].down - comp_[p1.y][p1.x][p2.y][p2.x].down;
+			break;
+		case Murakami::left:
+			score = comp_[p1.y][p1.x][sorted_comparation[p1][left][1].y][sorted_comparation[p1][left][1].x].left - comp_[p1.y][p1.x][p2.y][p2.x].left;
+			break;
+		default:
+			break;
+		}
+	}
+	else{
 
+	}
+	return score;
+}
 std::map <point_type, std::vector < std::vector<point_type>>> Murakami::sorted_comparation()
 {
 	auto const width = data_.split_num.first;
