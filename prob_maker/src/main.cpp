378dbc7f619ab5d07e855b768276473abaa4f794
Mizki SUZUMORI <mizki9577@gmail.com>
2014-08-31 17:46:31 +0900

    断片画像のサイズを指定できるようにし, 適切なサイズに縮小するようにした

--- prob_maker/src/main.cpp
+++ prob_maker/src/main.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-int constexpr hsplit_default = 16;
-int constexpr vsplit_default = 16;
+int constexpr piece_size_max = 128;
+int constexpr piece_size_min = 16;
+int constexpr split_max = 16;
+int constexpr split_min = 1;  // TODO: どうだったっけ?
+int constexpr image_size_max = 1024;
+int constexpr image_size_min = split_min * piece_size_min;
+
 int constexpr selectable_default = 16;
 int constexpr select_cost_default = 1;
 int constexpr change_cost_default = 1;
+int constexpr piece_width_default = -1;
+int constexpr piece_height_default = -1;
+int constexpr horizontal_split_default = -1;
+int constexpr vertical_split_default = -1;
 std::string const ext = ".ppm";
 
 int main(int argc, char* argv[])
 {
     std::vector<std::string> input_filenames;  // 入力ファイルの名前の配列
     std::string output_filename_base;  // 出力ファイル名のもと
-    int hsplit, vsplit;  // 分割数
+    cv::Size output_size;  // 出力画像の幅と高さ
+    int piece_width, piece_height;  // 断片画像の幅と高さ
+    int horizontal_split, vertical_split;  // 分割数
     int selectable;  // 選択可能回数
     int select_cost, change_cost;  // 選択コスト変換レート
 
     // コマンドラインオプションの定義
     boost::program_options::options_description options(argv[0]);
     options.add_options()
-        ("input,i",         boost::program_options::value<std::vector<std::string>>(&input_filenames)   ->multitoken() ->required(),            "入力ファイル名")
-        ("output,o",        boost::program_options::value<std::string>(&output_filename_base),                                                  "出力ファイル名")
-        ("hsplit,h",        boost::program_options::value<int>(&hsplit)                                 ->default_value(hsplit_default),        "横分割数")
-        ("vsplit,v",        boost::program_options::value<int>(&vsplit)                                 ->default_value(vsplit_default),        "縦分割数")
-        ("selectable,n",    boost::program_options::value<int>(&selectable)                             ->default_value(selectable_default),    "選択可能回数")
-        ("select_cost,s",   boost::program_options::value<int>(&select_cost)                            ->default_value(select_cost_default),   "選択コスト変換レート")
-        ("change_cost,c",   boost::program_options::value<int>(&change_cost)                            ->default_value(change_cost_default),   "交換コスト変換レート")
+        ("input,i",             boost::program_options::value<std::vector<std::string>>(&input_filenames)   ->multitoken() ->required(),                "入力ファイル名")
+        ("output,o",            boost::program_options::value<std::string>(&output_filename_base),                                                      "出力ファイル名")
+        ("piece_width,x",       boost::program_options::value<int>(&piece_width)                            ->default_value(piece_width_default),       "断片画像の幅")
+        ("piece_height,y",      boost::program_options::value<int>(&piece_height)                           ->default_value(piece_height_default),      "断片画像の高さ")
+        ("horizontal_split,h",  boost::program_options::value<int>(&horizontal_split)                       ->default_value(horizontal_split_default),  "横分割数")
+        ("vertical_split,v",    boost::program_options::value<int>(&vertical_split)                         ->default_value(vertical_split_default),    "縦分割数")
+        ("selectable,n",        boost::program_options::value<int>(&selectable)                             ->default_value(selectable_default),        "選択可能回数")
+        ("select_cost,s",       boost::program_options::value<int>(&select_cost)                            ->default_value(select_cost_default),       "選択コスト変換レート")
+        ("change_cost,c",       boost::program_options::value<int>(&change_cost)                            ->default_value(change_cost_default),       "交換コスト変換レート")
     ;
 
     // コマンドラインオプションのチェック
@@ -39,16 +53,18 @@ int main(int argc, char* argv[])
         boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), options_map);
         boost::program_options::notify(options_map);
     } catch (boost::program_options::error_with_option_name const& e) {
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
         std::cout << options << std::endl;
         return -1;
     }
 
     cv::Mat input_image;  // 入力画像
+    cv::Mat output_image;  // 出力画像
     std::string header;  // ヘッダ文字列
     std::vector<uchar> output_buffer;  // 出力画像バッファ
     std::string output_filename;  // 出力ファイルの名前
-    std::ofstream output_file; // 出力ファイルのストリーム
+    std::ofstream output_file;  // 出力ファイルのストリーム
+    cv::Size input_size;  // 入力画像のサイズ
     int count = 0;
 
     output_file.exceptions(std::ofstream::eofbit | std::ofstream::failbit | std::ofstream::badbit);
@@ -71,21 +87,83 @@ int main(int argc, char* argv[])
         }
         output_filename += ext;
 
-        // ヘッダ文字列の設定
-        header.erase();
-        header += (boost::format("# %1% %2%\n") % hsplit % vsplit).str();
-        header += (boost::format("# %1%\n") % selectable).str();
-        header += (boost::format("# %1% %2%\n") % select_cost % change_cost).str();
-
         // 画像読み込み
         input_image = cv::imread(input_filename);
         if (input_image.data == nullptr) {
             std::cerr << "Fatal: " << input_filename << " を読み込めませんでした" << std::endl;
             return -1;
         }
+        input_size.width = input_image.cols;
+        input_size.height = input_image.rows;
+
+        // 分割数, 断片画像サイズ, 全体のサイズの決定と縮小切抜
+        bool piece_size_valid = piece_size_min <= piece_width && piece_width <= piece_size_max && piece_size_min <= piece_height && piece_height <= piece_size_max;
+        bool split_valid = split_min <= horizontal_split && horizontal_split <= split_max && split_min <= vertical_split && vertical_split <= split_max;
+        if (piece_size_valid != split_valid) {
+            int max_width, max_height;
+            if (piece_size_valid) {
+                max_width = std::min(split_max * piece_width, image_size_max);
+                max_height = std::min(split_max * piece_height, image_size_max);
+            } else {
+                max_width = std::min(horizontal_split * piece_size_max, image_size_max);
+                max_height = std::min(vertical_split * piece_size_max, image_size_max);
+            }
+
+            std::cout << "入力画像を縮小します" << std::endl;
+            if (max_width < input_size.width && max_height < input_size.height) {
+                if (max_width < input_size.width && max_height < input_size.height) {
+                    if (input_size.width > input_size.height) {
+                        std::cout << "幅を基準として縮小します1" << std::endl;
+                        output_size.width = max_width;
+                        output_size.height = input_size.height * max_width / input_size.width;
+                    } else {
+                        std::cout << "高さを基準として縮小します1" << std::endl;
+                        output_size.width = input_size.width * max_height / input_size.height;
+                        output_size.height = max_height;
+                    }
+                } else if (max_width < input_size.width) {
+                    std::cout << "幅を基準として縮小します2" << std::endl;
+                    output_size.width = max_width;
+                    output_size.height = input_size.height * max_width / input_size.width;
+                } else if (max_height < input_size.height) {
+                    std::cout << "高さを基準として縮小します2" << std::endl;
+                    output_size.width = input_size.width * max_height / input_size.height;
+                    output_size.height = max_height;
+                }
+                std::cout << input_size.width << "x" << input_size.height << " -> " << output_size.width << "x" << output_size.height << std::endl;
+                cv::resize(input_image, output_image, output_size);
+            } else {
+                output_size = input_size;
+                output_image = input_image;
+            }
+
+            std::cout << output_size.width << "x" << output_size.height << " -> ";
+            if (piece_size_valid) {
+                horizontal_split = output_size.width / piece_width;
+                vertical_split = output_size.height / piece_height;
+                output_size.width -= output_size.width % piece_width;
+                output_size.height -= output_size.height % piece_height;
+            } else {
+                piece_width = output_size.width / horizontal_split;
+                piece_height = output_size.height / vertical_split;
+                output_size.width -= output_size.width % horizontal_split;
+                output_size.height -= output_size.height % vertical_split;
+            }
+            std::cout << output_size.width << "x" << output_size.height << std::endl;
+            output_image = output_image.colRange(0, output_size.width).rowRange(0, output_size.height);
+        } else {
+            std::cerr << "Fatal: " << "分割数と断片画像サイズのどちらかを指定してください" << std::endl;
+            return -1;
+        }
+
+        // ヘッダ文字列の設定
+        header.erase();
+        header += (boost::format("# %1% %2%\n") % horizontal_split % vertical_split).str();
+        header += (boost::format("# %1%\n") % selectable).str();
+        header += (boost::format("# %1% %2%\n") % select_cost % change_cost).str();
 
         // バッファへ書き込み
-        cv::imencode(ext, input_image, output_buffer, std::vector<int>(CV_IMWRITE_PXM_BINARY));
+        cv::imencode(ext, output_image, output_buffer, std::vector<int>(CV_IMWRITE_PXM_BINARY));
 
         // ヘッダ流し込み
         // 先頭の "P6\n" の後にヘッダ文字列を挿入する
