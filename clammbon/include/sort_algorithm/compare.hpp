c00e928a8f6178255f85c55192f16aa6818bafc3
吉川 千里 <tisato8899@mail.com>
2014-09-12 00:46:34 +0900

    compare　修正　とんこつラーメン通る

--- clammbon/include/sort_algorithm/compare.hpp
+++ clammbon/include/sort_algorithm/compare.hpp
@@ -8,7 +8,7 @@ point_type ul_choose(compared_type const& comp, point_type const ur, point_type 
 point_type dr_choose(compared_type const& comp, point_type const ul, point_type const ur, point_type const dl);
 point_type dl_choose(compared_type const& comp, point_type const ul, point_type const ur, point_type const dr);
 point_type ur_choose(compared_type const& comp, cr_set const& cr, int method, point_type const ul, point_type const dl, point_type const dr);
-uint64_t form_evaluate(compared_type const& comp_,std::vector<std::vector<point_type> > matrix);
-int get_kind_num(question_raw_data const& data_,std::vector<std::vector<point_type>> matrix, int x, int y);
+uint64_t form_evaluate(compared_type const& comp_,std::vector<std::vector<point_type> > const& matrix);
+int get_kind_num(question_raw_data const& data_,std::vector<std::vector<point_type>> const& matrix, int const x, int const y);
 
 #endif
