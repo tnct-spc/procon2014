28de8f76c6df434748a07709ce031dce8648e1ec
Fuga Kato <tresener.K@gmail.com>
2014-09-16 20:44:06 +0900

    question_typeのコンストラクタとかを殺す

--- clammbon/include/data_type.hpp
+++ clammbon/include/data_type.hpp
@@ -63,31 +63,6 @@ struct point_type
             );
     }
 
-    inline point_type above() const
-    {
-        return point_type{this->x, this->y - 1};
-    }
-    inline point_type below() const
-    {
-        return point_type{this->x, this->y + 1};
-    }
-    inline point_type left() const
-    {
-        return point_type{this->x - 1, this->y};
-    }
-    inline point_type right() const
-    {
-        return point_type{this->x + 1, this->y};
-    }
-    inline point_type move_to(char const c) const
-    {
-        switch(c) {
-            case 'U': return above(); break;
-            case 'D': return below(); break;
-            case 'L': return left();  break;
-            case 'R': return right(); break;
-            default:  return point_type{-1, -1}; break;
-        }
     inline point_type const up() const
     {
         return point_type{this->x, this->y - 1};
@@ -151,7 +126,7 @@ typedef cv::Mat_<cv::Vec3b>                  image_type;
 // [i][j]の位置に分割された画像(cv::Mat_<cv::Vec3b>)が入っている．
 typedef std::vector<std::vector<image_type>> split_image_type;
 
-struct question_data : private boost::noncopyable
+struct question_data //: private boost::noncopyable
 {
     int problem_id;
     std::string player_id;
@@ -175,26 +150,26 @@ struct question_data : private boost::noncopyable
     {
     }
 
-    question_data(question_data&& other)
-    {
-        *this = std::move(other);
-    }
+//    question_data(question_data&& other)
+//    {
+//        *this = std::move(other);
+//    }
 
     question_data()
     {
     }
 
-    question_data& operator=(question_data&& other)
-    {
-        this->problem_id = other.problem_id;
-        this->player_id = other.player_id;
-        this->size = std::move(other.size);
-        this->selectable = other.selectable;
-        this->cost_select = other.cost_select;
-        this->cost_change = other.cost_change;
-        this->block = std::move(other.block);
-        return *this;
-    }
+//    question_data& operator=(question_data&& other)
+//    {
+//        this->problem_id = other.problem_id;
+//        this->player_id = other.player_id;
+//        this->size = std::move(other.size);
+//        this->selectable = other.selectable;
+//        this->cost_select = other.cost_select;
+//        this->cost_change = other.cost_change;
+//        this->block = std::move(other.block);
+//        return *this;
+//    }
 
     question_data clone() const
     {
