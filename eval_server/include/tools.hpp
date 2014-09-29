f8e1add5a0af13288ec5539d6281db5c8293f353
Fuga Kato <tresener.K@gmail.com>
2014-09-29 21:07:15 +0900

    ansに対応(途中)

--- eval_server/include/tools.hpp
+++ eval_server/include/tools.hpp
@@ -7,13 +7,14 @@
 point_type const pos2int(std::string const pos);
 std::map<std::string, std::string> const post_req_to_map(std::string req);
 void percent_decode(std::string &s);
+std::vector<std::vector<point_type>> load_answer(std::string const& filename);
 inline std::string const path_to_ppm(std::string const problem_set, std::string const problem_id)
 {
     return PCS_PROBDIR + "/" + problem_set + "/problem/prob" + problem_id + ".ppm";
 }
 inline std::string const path_to_pos(std::string const problem_set, std::string const problem_id)
 {
-    return PCS_PROBDIR + problem_set + "/position/prob" + problem_id + ".pos";
+    return PCS_PROBDIR + "/" + problem_set + "/position/prob" + problem_id + ".pos";
 }
 
 #endif
