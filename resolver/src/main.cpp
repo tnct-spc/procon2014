385fc01344c7ac90b435b2faa99f0c5f6e85fcb1
kinironote <kinironote@gmail.com>
2014-10-03 18:39:53 +0900

    アルゴリズムのテストブランチ

--- resolver/src/main.cpp
+++ resolver/src/main.cpp
@@ -22,120 +22,131 @@
 class analyzer : boost::noncopyable
 {
 public:
-    explicit analyzer(int const problem_id, std::string const& player_id)
-        : netclient_(), first_(true), problem_id_(problem_id), player_id_(player_id)
-    {
-    }
-    virtual ~analyzer() = default;
+	explicit analyzer(int const problem_id, std::string const& player_id)
+		: netclient_(), first_(true), problem_id_(problem_id), player_id_(player_id)
+	{
+	}
+	virtual ~analyzer() = default;
 
-    question_data operator() ()
-    {
-        if(first_)
-        {
-            first_ = false;
-            return first_process(problem_id_, player_id_);
-        }
-        else
-        {
-            // TODO: yrange5の結果を待つなど
-            return question_data{};
-        }
-    }
+	question_data operator() ()
+	{
+		if (first_)
+		{
+			first_ = false;
+			return first_process(problem_id_, player_id_);
+		}
+		else
+		{
+			// TODO: yrange5の結果を待つなど
+			return question_data{};
+		}
+	}
 
+	void get_probnum(std::string const& in_probnum){
+		probnum = in_probnum;
+	}
 private:
-    question_data first_process(int const problem_id, std::string const& player_id)
-    {
-        question_raw_data raw;
+	std::string probnum;
+
+	question_data first_process(int const problem_id, std::string const& player_id)
+	{
+		question_raw_data raw;
 #if 1
-        // ファイルから
-        std::string const path("prob01.ppm");
-        raw = reader_.from_file(path);
+		// ファイルから
+		std::string const path(probnum);
+		raw = reader_.from_file(path);
 #else
-        // ネットワーク通信から
-        std::string const data = netclient_.get_problem(01).get();
-        raw = reader_.from_data(data);
+		// ネットワーク通信から
+		std::string const data = netclient_.get_problem(01).get();
+		raw = reader_.from_data(data);
 #endif
-        question_data formed = {
-            problem_id,
-            player_id,
-            raw.split_num,
-            raw.selectable_num,
-            raw.cost.first,
-            raw.cost.second,
-            std::vector<std::vector<point_type>>()
-        };
+		question_data formed = {
+			problem_id,
+			player_id,
+			raw.split_num,
+			raw.selectable_num,
+			raw.cost.first,
+			raw.cost.second,
+			std::vector<std::vector<point_type>>()
+		};
 
-        // 2次元画像に分割
-        auto splitted = splitter().split_image(raw);
+		// 2次元画像に分割
+		auto splitted = splitter().split_image(raw);
 
-        // yrangeなどの実行
-        auto sorter_resolve = sorter_(raw, splitted);
-        formed.block = std::move(sorter_resolve);
+		// yrangeなどの実行
+		auto sorter_resolve = sorter_(raw, splitted);
+		formed.block = std::move(sorter_resolve);
 
 		// 手作業用のウィンドウの作成
 		auto future = gui::make_mansort_window(splitted, "exchange");
 
-        // TODO: yrange5の実行
+		// TODO: yrange5の実行
 
-        return std::move(formed);
-    }
+		return std::move(formed);
+	}
 
-    int const problem_id_;
-    std::string const player_id_;
+	int const problem_id_;
+	std::string const player_id_;
 
-    bool first_;
-    ppm_reader reader_;
-    network::client netclient_;
-    pixel_sorter<Murakami> sorter_;
+	bool first_;
+	ppm_reader reader_;
+	network::client netclient_;
+	pixel_sorter<Murakami> sorter_;
 };
 
 question_data convert_block(question_data const& data)
 {
-    auto res = data.clone();
+	auto res = data.clone();
 
-    for(int i = 0; i < data.size.second; ++i)
-    {
-        for(int j = 0; j < data.size.first; ++j)
-        {
-            auto const& target = data.block[i][j];
-            res.block[target.y][target.x] = point_type{j, i};
-        }
-    }
+	for (int i = 0; i < data.size.second; ++i)
+	{
+		for (int j = 0; j < data.size.first; ++j)
+		{
+			auto const& target = data.block[i][j];
+			res.block[target.y][target.x] = point_type{ j, i };
+		}
+	}
 
-    return res;
+	return res;
 }
 
 // 問題の並び替えパズル自体は，人間が行うほうがいいかもしれない．
 
 int main()
 {
-    auto const ploblemid = 1;
-    auto const token     = "3935105806";
+	std::string probnum;
+	auto const ploblemid = 1;
+	auto const token = "3935105806";
 
-    analyzer        analyze(ploblemid, token);
-    algorithm_2       algo;
-    network::client client;
+	analyzer        analyze(ploblemid, token);
+	algorithm_2       algo;
+	network::client client;
 
-    // 原画像推測部
-    auto const suggested = analyze();
-    auto const converted = convert_block(suggested);
+	std::cout << "prob number:";
+	std::cin >> probnum;
+	analyze.get_probnum(probnum);
 
-    // 手順探索部
-    algo.reset(converted);
-    auto const answer = algo.get();
+	// 原画像推測部
+	auto const suggested = analyze();
+	auto const converted = convert_block(suggested);
 
-    // 1発目の提出
-    auto submit_result = client.submit(ploblemid, token, answer.get());
-    std::cout << submit_result.get() << std::endl;
+	// 手順探索部
+	algo.reset(converted);
+	auto const answer = algo.get();
 
-    // 2発目以降の提出など(yrange5)
-    auto const second_suggested = analyze();
-    algo.reset(convert_block(second_suggested));
-    auto const second_answer = algo.get();
+	// 1発目の提出
+	auto submit_result = client.submit(ploblemid, token, answer.get());
+	std::cout << submit_result.get() << std::endl;
 
-    auto submit_second_result = client.submit(ploblemid, token, second_answer.get());
-    std::cout << submit_second_result.get() << std::endl;
+	// 2発目以降の提出など(yrange5)
+	//auto const second_suggested = analyze();
+	//algo.reset(convert_block(second_suggested));
+	//auto const second_answer = algo.get();
+	//
+	//auto submit_second_result = client.submit(ploblemid, token, second_answer.get());
+	//std::cout << submit_second_result.get() << std::endl;
 
-    return 0;
+	std::cout << "■■■algorythm end■■■";
+	return 0;
 }
 
