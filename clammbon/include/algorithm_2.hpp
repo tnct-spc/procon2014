004b779c6e09f48eaf24364f5e34796f38b24f46
kinironote <kinironote@gmail.com>
2014-10-07 16:45:31 +0900

    Refactoring

--- clammbon/include/algorithm_2.hpp
+++ clammbon/include/algorithm_2.hpp
@@ -2,31 +2,38 @@
 #define RESOLVER_ALGORITHM_2_HPP
 
 #include <vector>
-//#include <iostream>
-//#include <boost/optional.hpp>
+#include <iomanip>
 #include "data_type.hpp"
 
+//û”[”z—ñ
 class heap
 {
-private:
-	int y, x;
-	long long int pos = 0;
-	long long int size = 0;
-	int size_million = 1;
-	std::vector<long long int> heaptable;
-	std::vector<int> cost;
-	std::vector< std::vector<int> > history;
-	std::vector<int>history_limit;
-	std::vector< std::vector<int> > table;
 public:
 	heap();
 	~heap();
 
-	void setup(int y, int x);
-	void pop(int *in_cost, int in_table[], std::vector<int> &in_history, int *history_limit);
-	void push(int *in_cost, int in_table[], std::vector<int> &in_history, int *history_limit);
+	//‰Šú‰»
+	void setup(const int* y, const int* x);
+	//©æo
+	void pop(int *in_cost, std::vector<int> &in_table, std::vector<int> &in_history, int *in_history_limit);
+	//¨‘}“ü
+	void push(int *in_cost, std::vector<int> &in_table, std::vector<int> &in_history, int *in_history_limit);
+
+private:
+	int yx, y, x;
+	int pos = 0;
+	int size = 0;
+	int sizemaxcount = 1;
+	std::vector<int> heaptable;
+	std::vector<int> cost;
+	std::vector< std::vector<int> > history;
+	std::vector<int>history_limit;
+	std::vector< std::vector<int> > table;
 };
 
+
+
+//ƒAƒ‹ƒSƒŠƒYƒ€
 class algorithm_2
 {
 public:
@@ -35,37 +42,55 @@ public:
 	algorithm_2();
 	~algorithm_2();
 
-	auto get()->boost::optional<return_type>;
+	//‰Šú‰»
 	void reset(question_data const& data);
 
+	//Às
+	auto get() -> boost::optional<return_type>;
+
 private:
-	boost::optional<question_data> data_;
-	int cost_select;
-	int cost_change;
-	int *table;
-	int *sub_table;
-	int goal = 1;
-	int sentaku = 1;
-	int coukan = 1;
-	int size_million_history = 1;
-	int size_million_keiro = 1;
 	heap harray;
+	boost::optional<question_data> data_;
+	const int goal = 10;
+	const int sentaku = 10;
+	const int coukan = 10;
+	//1,1,1		ƒm[ƒ}ƒ‹
+	//10,5,3	‘I‘ğ‚ª’Z‚¢‚Ì‚ğì‚ê‚½
+	//10 8 4
+	//10 14 6
 	int cost;
-	std::vector<int> history;
-	std::vector<int> sub_history;
-	std::vector<int> keiro;
-	int history_limit = 0;
-	int sub_history_limit = 0;
-	int keiro_count = 0;
 	int size;
 	int y_size;
 	int x_size;
+	int selectcost;
+	int changecost;
+	int sizemaxhistory = 1;
+	int sizemaxkeiro = 1;
+	int history_limit = 0;
+	int sub_history_limit = 0;
+	int keiro_count = 0;
+	int G, S, C;
+	std::vector<int> table;
+	std::vector<int> sub_table;
+	std::vector<int> history;
+	std::vector<int> sub_history;
+	std::vector<int> keiro;
+	std::vector<int> root1;//use shorting
+	std::vector<int> root2;//use shorting
+	int sizemaxroot1 = 1;
+	int sizemaxroot2 = 1;
 
+	//ƒAƒ‹ƒSƒŠƒYƒ€‚ÌÀs
+	void puzzle(std::vector<int> &out_history, int &out_cost);
+	//‡”Ô‚É’Tõ‚ğŠJn‚·‚é
 	void prescanning();
+	//’Tõ
 	void scanning(int i, int j, int i_pre, int j_pre, int URDL);
+
+	//Œo˜H‚ğ’Z‚­‚·‚é
 	void shorting();
-	void reverse(int*);
-	int puzzle(std::vector<int> &out_history, int &out_cost);//’Tõ
+	//Œo˜H”½“]
+	void reverse(int* x);
 };
 
 #endif
--- clammbon/src/algorithm_2.cpp
+++ clammbon/src/algorithm_2.cpp
@@ -1,18 +1,20 @@
-//////////////////////////
-//最優先探索アルゴリズム//
-//////////////////////////
+////////////////////////////////////////////
+//Happy Algorithm (最優先探索アルゴリズム)//
+////////////////////////////////////////////
 
 #define debug
 //#define test1
-//#define nosaiki
-//#define debugdebug
+//#define test2
+//#define debugdebug1
 
 //インクルードファイル
 #ifdef debug
+#include <iostream>
+#include <boost/timer/timer.hpp>
 #endif
 #include "algorithm_2.hpp"
-#include <cmath>
-#include <boost/timer/timer.hpp>
+
+/*■■配列■■*/
 
 //コンストラクタ
 heap::heap(){
@@ -21,49 +23,50 @@ heap::heap(){
 heap::~heap(){
 }
 
-
-void heap::setup(int in_y, int in_x){
-	y = in_y;
-	x = in_x;
+void heap::setup(const int *in_y, const int *in_x){
+	y = *in_y;
+	x = *in_x;
+	yx = y*x;
+	//配列のリサイズ☆おおよそ
 	cost.resize(1000000);
 	history.resize(1000000);
 	history_limit.resize(1000000);
-	table.resize(1000000);
+	table.resize(1000000, std::vector<int>(yx));
 	heaptable.resize(1000000);
 }
-void heap::pop(int *in_cost, int *in_table, std::vector<int> &in_history, int *in_history_limit){
+void heap::pop(int *in_cost, std::vector<int> &in_table, std::vector<int> &in_history, int *in_history_limit){
 
-	long long int i = 0;
-	long long int me;
-	long long int top = pos;
+	int i;
+	int me;
+	int top = pos;
 
-	if (size > 1000000 * size_million - 1){
+	if (size > 1000000 * sizemaxcount - 1){
+		sizemaxcount++;
+		cost.resize(1000000 * sizemaxcount);
+		history.resize(1000000 * sizemaxcount);
+		history_limit.resize(1000000 * sizemaxcount);
+		table.resize(1000000 * sizemaxcount, std::vector<int>(y*x));
+		heaptable.resize(1000000 * sizemaxcount);
 #ifdef debug
-		std::cout << "><heap.cpp vector pass1,000,000" << std::endl;
+		std::cout << ">< heap.cpp vector pass1,000,000" << std::endl;
 #endif
-		size_million++;
-		cost.resize(1000000 * size_million);
-		history.resize(1000000 * size_million);
-		history_limit.resize(1000000 * size_million);
-		table.resize(1000000 * size_million);
-		heaptable.resize(1000000 * size_million);
 	}
 
 	//コスト挿入
 	cost[size] = *in_cost;
+
 	//履歴挿入
-	i = 0;
 	history[size].resize(*in_history_limit);
-	while (i < *in_history_limit){
+	for (i = 0; i < *in_history_limit; i++){
 		history[size][i] = (in_history[i]);
-		i++;
 	}
 	history_limit[size] = *in_history_limit;
+
 	//テーブル挿入
-	table[size].resize(y*x);
-	for (i = 0; i < y*x; i++){
-		table[size][i] = (in_table[i]);
+	for (i = 0; i < yx; i++){
+		table[size][i] = in_table[i];
 	}
+
 	//ヒープ配列挿入
 	while (1){
 		me = top;
@@ -85,30 +88,31 @@ void heap::pop(int *in_cost, int *in_table, std::vector<int> &in_history, int *i
 
 }
 
-void heap::push(int *out_cost, int *out_table, std::vector<int> &out_history, int *out_history_limit){
-	long long int out = heaptable[0];
-
-	long long int i = 0;
-	long long int me = 0;
-	long long int bottom = 0;
+void heap::push(int *out_cost, std::vector<int> &out_table, std::vector<int> &out_history, int *out_history_limit){
+	const int out = heaptable[0];
+	int i = 0;
+	int me = 0;
+	int bottom = 0;
 
 	pos--;
 	size--;
+
 	//コスト挿入
-	*out_cost = cost[heaptable[0]];
+	*out_cost = cost[out];
+
 	//履歴挿入
 	i = 0;
-	*out_history_limit = history_limit[heaptable[0]];
+	*out_history_limit = history_limit[out];
 	while (i < *out_history_limit){
-		out_history[i] = history[heaptable[0]][i];
+		out_history[i] = history[out][i];
 		i++;
 	}
-	//テーsブル挿入
-	for (i = 0; i < table[heaptable[0]].size(); i++){
-		out_table[i] = table[heaptable[0]][i];
+
+	//テーブル挿入
+	for (i = 0; i < yx; i++){
+		out_table[i] = table[out][i];
 	}
-	//std::cout << "num=" << heaptable[0] << std::endl;
-	//std::cout << "pos="<<pos<<" size="<<size << std::endl;
+
 	//ヒープ配列挿入
 	while (1){
 		me = bottom;
@@ -117,7 +121,6 @@ void heap::push(int *out_cost, int *out_table, std::vector<int> &out_history, in
 			if (cost[heaptable[bottom + 1]] < cost[heaptable[bottom + 2]]){
 				if (cost[heaptable[pos]] > cost[heaptable[bottom + 1]]){
 					bottom += 1;
-					//std::cout << "" << me << " << " << bottom << std::endl;
 					heaptable[me] = heaptable[bottom];
 				}
 				else{
@@ -127,7 +130,6 @@ void heap::push(int *out_cost, int *out_table, std::vector<int> &out_history, in
 			else{
 				if (cost[heaptable[pos]] > cost[heaptable[bottom + 2]]){
 					bottom += 2;
-					//std::cout << "" << me << " << " << bottom << std::endl;
 					heaptable[me] = heaptable[bottom];
 				}
 				else{
@@ -138,7 +140,6 @@ void heap::push(int *out_cost, int *out_table, std::vector<int> &out_history, in
 		else if (pos>bottom + 1){
 			if (cost[heaptable[pos]] > cost[heaptable[bottom + 1]]){
 				bottom += 1;
-				//std::cout << "" << me << " << " << bottom << std::endl;
 				heaptable[me] = heaptable[bottom];
 			}
 			else{
@@ -149,28 +150,14 @@ void heap::push(int *out_cost, int *out_table, std::vector<int> &out_history, in
 			break;
 		}
 	}
-	//std::cout << "me=" << me << "pos=" << pos << std::endl;
+
 	heaptable[me] = heaptable[pos];
 	heaptable.erase(heaptable.end() - 1);
 
 	size++;
 }
 
-
-
-
-
-
-
-
-
-//ここからアルゴリズムのコード
-
-
-
-
-
-
+/*■■アルゴリズム■■*/
 
 //コンストラクタ
 algorithm_2::algorithm_2()
@@ -180,8 +167,6 @@ algorithm_2::algorithm_2()
 //デストラクタ
 algorithm_2::~algorithm_2()
 {
-	delete[] table;
-	delete[] sub_table;
 }
 
 //初期化
@@ -196,89 +181,85 @@ void algorithm_2::reset(question_data const& data)
 	size = y_size*x_size;
 
 	// コストとレート
-	cost_select = data_->cost_select;
-	cost_change = data_->cost_change;
+	selectcost = data_->cost_select;
+	changecost = data_->cost_change;
+
 
 	// テーブル
 	//tableによそから貰ってきたマトリクスを整理して挿入
-	table = new int[size];//y*xサイズのテーブルを作る
-	sub_table = new int[size];//y*xサイズのテーブルを作る
+	table.resize(size);
+	sub_table.resize(size);
 	for (int y = 0; y_size > y; y++){
 		for (int x = 0; x_size > x; x++){
 			table[y * x_size + x] = data_->block[y][x].y * x_size + data_->block[y][x].x;
-			sub_table[y * x_size + x] = table[y * x_size + x];
 		}
 	}
 
-	//配列のりサイズ
-	history.resize(1000000);
-	sub_history.resize(1000000);
-	keiro.resize(1000000);
-
-	//変数の初期化
-	history_limit = 0;
-	sub_history_limit = 0;
-
+	//配列のリサイズ☆おおよそ
+	history.resize(10000);
+	sub_history.resize(10000);
+	keiro.resize(10000);
+	root1.resize(1024);
+	root2.resize(1024);
 	//heapの初期化
-	harray.setup(y_size, x_size);
-
+	harray.setup(&y_size, &x_size);
 }
 
 
 
 
 
-auto algorithm_2::get() -> boost::optional<answer_type>
+auto algorithm_2::get() -> boost::optional<return_type>
 {
-#ifdef debug
-	std::cout << "algorythm_2 start" << std::endl;
-	boost::timer::cpu_timer timer; // 時間計測を開始
-
-	std::cout << "x_size=" << x_size << ",y_size=" << y_size << std::endl;
-	for (int y = 0; y_size > y; y++){
-		for (int x = 0; x_size > x; x++){
-			std::cout << table[y * x_size + x] << ",";
-		}
-		std::cout << std::endl;
-	}
-#endif
 	std::vector<int> get_history(0);
 	int get_cost = 0;
 
 #ifdef debug
+	std::cout << "algorythm_2 start" << std::endl;
+	std::cout << "y_size=" << y_size << ",x_size=" << x_size << std::endl;
+	for (int y = 0; y_size > y; y++){
+		for (int x = 0; x_size > x; x++){
+			std::cout << std::setw(3) << table[y * x_size + x] << ",";
+		}
+		std::cout << std::endl;
+	}
 	//パズルスタート
 	std::cout << "start" << std::endl;
+	boost::timer::cpu_timer timer; // 時間計測を開始
 #endif
+
 	puzzle(get_history, get_cost);	//実行
+
 #ifdef debug
-	std::cout << "end" << std::endl;
 	std::string result = timer.format();
 	std::cout << "\n処理時間:" << result << std::endl;
-	for (int i = 0; i < get_history.size(); i++){
+	std::cout << "end" << std::endl;
+	for (unsigned int i = 0; i < get_history.size(); i++){
 		switch (get_history[i]){
-			case 16:
-			case 20:
-				std::cout << ",U";
-				break;
-			case 17:
-			case 21:
-				std::cout << ",R";
-				break;
-			case 18:
-			case 22:
-				std::cout << ",D";
-				break;
-			case 19:
-			case 23:
-				std::cout << ",L";
-				break;
-			default:
-				std::cout << "," << get_history[i];
-				break;
+		case 16:
+		case 20:
+			std::cout << ",U";
+			break;
+		case 17:
+		case 21:
+			std::cout << ",R";
+			break;
+		case 18:
+		case 22:
+			std::cout << ",D";
+			break;
+		case 19:
+		case 23:
+			std::cout << ",L";
+			break;
+		default:
+			std::cout << "," << get_history[i];
+			break;
 		}
 	}
+	std::cout << std::endl;
 	int debug_S = 0, debug_C = 0;
-	for (int i = 0; i < get_history.size(); i++){
+	for (unsigned int i = 0; i < get_history.size(); i++){
 		if (get_history[i] < 16){
 			debug_S += 1;
 		}
@@ -287,20 +268,21 @@ auto algorithm_2::get() -> boost::optional<answer_type>
 		}
 	}
 	debug_S /= 2;
-	std::cout << "\ncost=" << debug_S * cost_select + debug_C * cost_change << " S: " << debug_S << " C: " << debug_C << std::endl;
+	std::cout << "selectcost=" << selectcost << ",changecost=" << changecost << std::endl;
+	std::cout << "cost=" << debug_S * selectcost + debug_C * changecost << " S: " << debug_S << " C: " << debug_C << std::endl;
 #endif
 	//解をanswer_typeにして返す
 	answer_type answerlist;
 	point_type position;
 	std::ostringstream stream;
-	answerlist.list.resize(1024);//★☆
+	answerlist.list.resize(1024);//★適当
 	int pos = 0;
-	for (int count=0; pos < get_history.size();count++){
+	for (int count = 0; pos < get_history.size(); count++){
 		position.y = get_history[pos];
 		pos++;
 		position.x = get_history[pos];
 		pos++;
-		while(pos < get_history.size() && 16 <= get_history[pos]){
+		while (pos < get_history.size() && 16 <= get_history[pos]){
 			switch (get_history[pos]){
 			case 16:
 			case 20:
@@ -332,23 +314,15 @@ auto algorithm_2::get() -> boost::optional<answer_type>
 
 
 
-
-
-
-
-
-
-
 //探索
-int algorithm_2::puzzle(std::vector<int> &out_history, int &out_cost){
+void algorithm_2::puzzle(std::vector<int> &out_history, int &out_cost){
 	int count, i;
-
 
 	while (1){
 		algorithm_2::prescanning(); //探索
 #ifdef test1
-		//★★★試験的な実験★★★
-		//連続で来たおんなじのは飛ばす
+		//☆試験的な実験☆
+		//同じテーブルが連続で来たら飛ばす
 		while (1){
 			harray.push(&cost, table, history, &history_limit);
 			int buff = 0;
@@ -359,67 +333,44 @@ int algorithm_2::puzzle(std::vector<int> &out_history, int &out_cost){
 				sub_table[i] = table[i];
 			}
 			if (buff != size){
-				//前回と違うマス位置
+				//前回と違うテーブル
 				break;
 			}
 		}
-		//★★★★ここまで★★★★
+		//☆ここまで☆
 #else
-
+		//取得
+		harray.push(&cost, table, history, &history_limit);
 #endif
-		while (1){
-			while (1){
-				harray.push(&cost, table, history, &history_limit);
-				int buff = 0;
-				for (i = 0; i < size; i++){
-					if (sub_table[i] == table[i])buff++;
-				}
-				for (i = 0; i < size; i++){
-					sub_table[i] = table[i];
-				}
-				if (buff != size){
-					//前回と違うマス位置
-					break;
-				}
-			}
-			//完成判定
-			count = 0;
-			for (i = 0; i<size; i++){
-				if (table[i] == i) count++;
-			}
-			if (count == size){
-				//完成
-				out_cost = cost;
-				out_history.resize(history_limit);
-				for (i = 0; i < history_limit; i++){
-					out_history[i] = history[i];
-				}
-				return 0;
-			}
-			int S = 0;
-			for (i = 0; i < history_limit; i++){
-				if (history[i] < 16){
-					S++;
-				}
-			}
-			S /= 2;
-			if (S < 4) break;
+		//完成判定
+		count = 0;
+		for (i = 0; i<size; i++){
+			if (table[i] == i) count++;
 		}
-		//debug
-		
-		for (int i = 0; i < 3; i++){
-			for (int j = 0; j < 6; j++){
-				std::cout << table[i * 6 + j] << ",";
+		if (count == size){
+			//完成
+			out_cost = cost;
+			out_history.resize(history_limit);
+			for (i = 0; i < history_limit; i++){
+				out_history[i] = history[i];
 			}
-			std::cout<<std::endl;
+			return;
+		}
+#ifdef debugdebug1
+		//debug
+		for (int i = 0; i < y_size; i++){
+			for (int j = 0; j < x_size; j++){
+				std::cout << table[i * x_size + j] << ",";
+			}
+			std::cout << std::endl;
 		}
 		for (int i = 0; i < history_limit; i++){
 			std::cout << history[i] << ",";
 		}
 		std::cout << std::endl;
-		int f;
-		std::cin >> f;
-		
+		int debugdebug1;
+		std::cin >> debugdebug1;
+#endif
 	}
 }
 
@@ -437,10 +388,9 @@ void algorithm_2::prescanning(){
 
 //走査
 void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
-	int i, j, buff;
+	int i, buff;
 	bool feel, feeled;
 	//コスト
-	int G, S, C;
 #ifdef debugdebug
 	for (i = 0; i < keiro_count; i++){
 		std::cout << keiro[i] << ",";
@@ -457,16 +407,15 @@ void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
 		}
 		std::cout << std::endl;
 	}
-	std::cout << std::endl;std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl;
+	std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl; std::cout << std::endl;
 	std::cin >> i;
 	//_sleep(30);
 #endif
-	if (keiro_count > 1000000 * size_million_keiro){
-		size_million_keiro++;
-		history.resize(1000000 * size_million_keiro);
-		sub_history.resize(1000000 * size_million_keiro);
+	if (keiro_count > 10000 * sizemaxkeiro){
+		sizemaxkeiro++;
+		keiro.resize(10000 * sizemaxkeiro);
 #ifdef debug
-		std::cout << "><search.cpp keiro vector pass1,000,000" << std::endl;
+		std::cout << ">< search.cpp keiro vector pass10,000" << std::endl;
 #endif
 	}
 #ifdef nosaiki //これで再帰無しの探索が出来る
@@ -596,13 +545,14 @@ void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
 #ifdef nosaiki
 	}
 #endif
-	if ((y != y_before || x != x_before)){//一番最初以外/* && ◆◆上位互換が存在しない(未実装)◆◆*/
-		if (history_limit > 1000000 * size_million_history){
-			size_million_history++;
-			history.resize(1000000 * size_million_history);
-			sub_history.resize(1000000 * size_million_history);
+	if ((y != y_before || x != x_before)) //一番最初以外/* && ◆◆上位互換が存在しない(未実装)◆◆*/
+	{
+		if (history_limit > 10000 * sizemaxhistory){
+			sizemaxhistory++;
+			history.resize(10000 * sizemaxhistory);
+			sub_history.resize(10000 * sizemaxhistory);
 #ifdef debug
-			std::cout << "><search.cpp history vector pass1,000" << std::endl;
+			std::cout << ">< search.cpp history vector pass10,000" << std::endl;
 #endif
 		}
 
@@ -616,30 +566,13 @@ void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
 		for (i = 0; i < sub_history_limit; i++){
 			sub_history[i] = history[i];
 		}
-		/*
-		std::cout << "strt" << std::endl;
-		std::cout << " limit=" << subhistory_limit << std::endl;
-		for (i = 0; i < subhistory_limit; i++){
-		std::cout << subhistory[i] << ",";
-		}
-		std::cout << std::endl;
-		*/
 		shorting();
-		/*
-		std::cout << "\nend" << std::endl;
-		std::cout << " limit=" << subhistory_limit << std::endl;
-		for (i = 0; i < subhistory_limit; i++){
-		std::cout << subhistory[i] << ",";
-		}
-		std::cout << std::endl;
-		std::cin >> i;
-		*/
 		//■■■■■■■■■■■■
 		//cost = 選択コスト+交換コスト+ゴールまでの距離;
 		G = 0;
 		S = 0;
 		C = 0;
-		//取り敢えずゴールまでの距離を入れておく
+		//ゴールまでの距離
 		for (i = 0; i < size; i++){
 			G += abs(table[i] / x_size - i / x_size) + abs(table[i] % x_size - i % x_size);
 		}
@@ -657,20 +590,18 @@ void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
 		C *= coukan;
 		cost = G + S + C;
 
-		//1,1,1		ノーマル
-		//10,5,3	選択が短いのを作れた
-		//10 8 4
-		//10 14 6
-		harray.pop(&cost, table, sub_history, &sub_history_limit);
-		/*
+#ifdef test2
 		//★最大選択回数より大きいならやめる
 		if (S / 15 < 3){
-		harray.pop(&cost, table, subhistory, &subhistory_limit);
+			harray.pop(&cost, table, subhistory, &subhistory_limit);
 		}
-		*/
+#else
+		//取得
+		harray.pop(&cost, table, sub_history, &sub_history_limit);
+#endif
 		history_limit -= keiro_count;
 	}
-	//全部見終わったら、dataを元通りにして返す(?)
+	//全部見終わったらtableを元通りにして返す
 	buff = table[y * x_size + x];
 	table[y * x_size + x] = table[y_before * x_size + x_before];
 	table[y_before * x_size + x_before] = buff;
@@ -681,33 +612,17 @@ void algorithm_2::scanning(int y, int x, int y_before, int x_before, int URDL){
 
 	y = y_before;
 	x = x_before;
-	//up
+	//再帰の上の階層に上がる
 }
 void algorithm_2::shorting(){
-	int i;
 	//経路をまとめて短縮する
-	int x1_head, y1_head, root1_count, y1_tail, x1_tail; std::vector<int> root1;
-	int x2_head, y2_head, root2_count, y2_tail, x2_tail; std::vector<int> root2;
+	int i;
+	int x1_head, y1_head, root1_count, y1_tail, x1_tail;
+	int x2_head, y2_head, root2_count, y2_tail, x2_tail;
 	int start, count = 0, end;
-	root1.resize(256);//このくらいでいいかな
-	root2.resize(256);
-	/*
-	std::cout << "keiro " << std::endl;
 
-	for (int g = 0; g < 64; g++){
-	std::cout << search_pattern_keiro[kaisou][count][g] << ",";
-	}
+	while (count + 5<sub_history_limit){
 
-	std::cout << "@" << std::endl;
-	*/
-	while (count + 3<sub_history_limit){
-		/*
-		std::cout << "c=" << count << " l=" << subhistory_limit << std::endl;
-		for (i = 0; i < subhistory_limit; i++){
-		std::cout << subhistory[i] << ",";
-		}
-		std::cin >> i;
-		*/
 		root1_count = 0;
 		root2_count = 0;
 		start = count;
@@ -716,7 +631,14 @@ void algorithm_2::shorting(){
 		x1_head = sub_history[count];
 		count++;
 		while (1){
-			if (!(count<sub_history_limit && sub_history[count] >= 16 && sub_history[count] <= 23)) break;//(-1または0からsize)以外
+			if (!(count<sub_history_limit && sub_history[count] >= 16 && sub_history[count] <= 23)) break;
+			if (root1_count > 1024 * sizemaxroot1 - 1){
+				sizemaxroot1++;
+				root1.resize(1024 * sizemaxroot1);
+#ifdef debug
+				std::cout << ">< search.cpp root1 vector pass10,000" << std::endl;
+#endif
+			}
 			root1[root1_count] = sub_history[count];
 			root1_count++;
 			count++;
@@ -731,6 +653,13 @@ void algorithm_2::shorting(){
 		end++;
 		while (1){
 			if (!(end < sub_history_limit && sub_history[end] >= 16 && sub_history[end] <= 23)) break;
+			if (root2_count > 1024 * sizemaxroot2 - 1){
+				sizemaxroot2++;
+				root2.resize(1024 * sizemaxroot2);
+#ifdef debug
+				std::cout << ">< search.cpp root2 vector pass10,000" << std::endl;
+#endif
+			}
 			root2[root2_count] = sub_history[end];
 			root2_count++;
 			end++;
@@ -798,7 +727,6 @@ void algorithm_2::shorting(){
 		}
 		//Rule1
 		if (y1_tail == y2_head && x1_tail == x2_head){
-			//std::cout << "rulu1" << std::endl;
 			for (i = 0; i < root2_count; i++){
 				sub_history[count] = root2[i];
 				count++;
@@ -811,7 +739,6 @@ void algorithm_2::shorting(){
 		}
 		//Rule2
 		else if (y1_tail == y2_tail && x1_tail == x2_tail && root2_count == 1){
-			//std::cout << "rulu2" << std::endl;
 			//root2の各方向を反転
 			reverse(&root2[0]);
 			sub_history[count] = root2[0];
@@ -824,7 +751,6 @@ void algorithm_2::shorting(){
 		}
 		//Rule3
 		else if (y1_head == y2_head && x1_head == x2_head && root1_count == 1){
-			//std::cout << "rulu3" << std::endl;
 			//root1の各方向を反転
 			reverse(&root1[0]);
 			sub_history[start] = y1_tail;
@@ -845,7 +771,6 @@ void algorithm_2::shorting(){
 		}
 		//Rule4
 		else if (y1_head == y2_tail && x1_head == x2_tail && root1_count == 1 && root2_count == 1){
-			//std::cout << "rulu4" << std::endl;
 			//root1の各方向を反転
 			reverse(&root1[0]);
 			//root2の各方向を反転
