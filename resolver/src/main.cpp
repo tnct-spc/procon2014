4b8824a9b0009e0197e5dae2a3de6b7b6ffb56f2
Godai Azuma <godai0519@gmail.com>
2014-10-12 00:56:48 +0900

    Threadによる実行のイメージはこれ．なお諸事情により，第1発目の自動投稿を削除．追加し直したい．
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

--- resolver/src/main.cpp
+++ resolver/src/main.cpp
@@ -77,67 +77,65 @@ public:
         // 原画像推測部
         yrange2 yrange2_(raw_data_, image_comp);
         Murakami murakami_(raw_data_, image_comp);
-		
-		//std::vector<point_type> yrange2_resolve;
-		//if (!yrange2_().empty())yrange2_resolve = yrange2_()[0].points;
-		auto yrange2_resolve = yrange2_();
-        auto murakami_resolve = murakami_()[0].points;
 
-		for (auto const& one_y2 : yrange2_resolve)
-		{
-			data_.block = one_y2.points;
-			if (!data_.block.empty())manager.add(convert_block(data_)); // 解答
-			break;
-		}
-
-        // 画面表示をいくつか(yrange/Murakmi/yrange5/algo2 etc.)
+        // GUI Threadの起動
         gui::manager gui_thread(
             [this, &manager](std::vector<std::vector<point_type>> const& data)
             {
+                // 回答としてマーク -> 回答ジョブに追加
                 auto clone = data_.clone();
                 clone.block = data;
                 manager.add(convert_block(clone));
             });
-		
-		//yrange2
-		if (!yrange2_resolve.empty())
-		{
-			for (int y2 = yrange2_resolve.size() - 1; y2 > 0; --y2)
-			{
+
+        // YRange2 -> YRange5 Thread
+        boost::thread y_thread(
+            [&]()
+            {
+                // YRange2
+                auto yrange2_resolve = yrange2_();
+                if (!yrange2_resolve.empty())
+		        {
+			        for (int y2 = yrange2_resolve.size() - 1; y2 > 0; --y2)
+			        {
+                        gui_thread.push_back(
+                            boost::bind(gui::make_mansort_window, split_image_, yrange2_resolve.at(y2).points, "yrange2")
+                            );
+			        }
+		        }
+                
+                // YRange5
+                auto yrange5_resolve = yrange5(raw_data_, image_comp)(yrange2_.sorted_matrix());
+                if (!yrange5_resolve.empty())
+		        {
+			        for (int y5 = yrange5_resolve.size() - 1; y5 > 0; --y5)
+			        {
+                        gui_thread.push_back(
+                            boost::bind(gui::make_mansort_window, split_image_, yrange5_resolve.at(y5).points, "yrange5")
+                            );
+			        }
+                }
+            });
+
+        // Murakami Thread
+        boost::thread m_thread(
+            [&]()
+            {
+                // Murakami
+                auto murakami_resolve = murakami_()[0].points;
                 gui_thread.push_back(
-                    boost::bind(gui::make_mansort_window, split_image_, yrange2_resolve.at(y2).points, "yrange2")
-                    );
-			}
-		}
-
-		//yrange5
-		auto yrange5_resolve = yrange5(raw_data_, image_comp)(yrange2_.sorted_matrix());
-		if (!yrange5_resolve.empty())
-		{
-			for (int y5 = yrange5_resolve.size() - 1; y5 > 0; --y5)
-			{
-                gui_thread.push_back(
-                    boost::bind(gui::make_mansort_window, split_image_, yrange5_resolve.at(y5).points, "yrange5")
-                    );
-			}
-		}
-
-		//murakami
-		if (!murakami_resolve.empty())
-        {
-            gui_thread.push_back(
-                boost::bind(gui::make_mansort_window, split_image_, murakami_resolve, "Murakami")
+                    boost::bind(gui::make_mansort_window, split_image_, murakami_resolve, "Murakami")
                 );
-        }
+            });
 
-		//どっちもダメだった時
-		if (yrange2_resolve.empty() && yrange5_resolve.empty()/*&& murakami_resolve.empty()*/){
-            gui_thread.push_back(
-                boost::bind(gui::make_mansort_window, split_image_, "Yor are the sorter!!! Sort this!")
-                );
-		}
+        gui_thread.push_back(
+            boost::bind(gui::make_mansort_window, split_image_, "Yor are the sorter!!! Sort this!")
+            );
 
-        gui_thread.wait_all_window();        
+        // 各Threadの待機
+        y_thread.join();
+        m_thread.join();
+        gui_thread.wait_all_window();   
     }
 
     std::string submit(answer_type const& ans) const
