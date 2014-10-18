187dc5f27f276563d59410f6c52257bca2a7f935
hoge <hoge@hoge.com>
2014-10-18 12:11:21 +0900

    公式で動くように修正
    Signed-off-by: hoge <hoge@hoge.com>

--- clammbon/include/network.hpp
+++ clammbon/include/network.hpp
@@ -11,7 +11,7 @@ namespace network
     class client
     {
     private:
-        typedef boost::network::http::basic_client<boost::network::http::tags::http_async_8bit_tcp_resolve,1 ,1> client_type;
+        typedef boost::network::http::basic_client<boost::network::http::tags::http_async_8bit_udp_resolve,1 ,0> client_type;
 
         std::string const server_host_;
         std::string const problem_path_;
